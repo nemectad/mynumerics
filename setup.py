39b6340b4d88c55a3b041290145d8739f2ba23e2
nemectad <tada.nemec@seznam.cz>
2022-02-23 22:19:09 +0100

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['mynumerics'],
     # Needed for dependencies
-    install_requires=['numpy', 'scipy', 'h5py'],
+    install_requires=['numpy', 'scipy', 'h5py', 'matplotlib'],
     # *strongly* suggested for sharing
     version='0.1.2',
     # The license can be anything you like
